dcd0c553dd6a8f0e93385d2adcf79a57738c95e2
awo-ali <awoali@hotmail.co.uk>
2022-08-13 17:54:53 +0100

    refresh button

--- src/component/Button/Button.jsx
+++ src/component/Button/Button.jsx
@@ -2,32 +2,12 @@ import React from 'react'
 import './Button.scss';
 
 
-const Button = () => {
+const Button = ({getWeather}) => {
 
-    // const handleClick = () => {
-    //     setFetching()
-    //     getCurrentLocationData()
-    // }
-
-    const getCurrentLocationData = () => {
-        // if (navigator.geolocation) {
-        //     navigator.geolocation.getCurrentPosition(position => {
-        //         let latitude = position.coords.latitude
-        //         let longitude =  position.coords.longitude
-        //         setLocation({position: {latitude, longitude}})
-        //         getWeatherData(latitude, longitude)
-        //     })
-        if (navigator.geolocation) {
-            navigator.geolocation.watchPosition(function(position) {
-              console.log("Latitude is :", position.coords.latitude);
-              console.log("Longitude is :", position.coords.longitude);
-            });
-        }   
-    }
 
   return (
     <div className="Button">
-   <button onClick={getCurrentLocationData}>Get Location</button>
+   <button onClick={getWeather}>Get Location</button>
     </div>
   );
 }
