c79b552424929237e84dfe68e4da04cfca3af7f6
awo-ali <awoali@hotmail.co.uk>
2022-08-14 18:55:43 +0100

    fetch current and forecast weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
 import React, { useEffect, useState } from "react";
 import "./App.scss";
 import Button from "./component/Button/Button";
+import { WEATHER_API_URL, WEATHER_API_KEY} from './api';
+
 
 const App = () => {
 
-  const [weather, setWeather] = useState([]);
+  const [currentWeather, setCurrentWeather] = useState([]);
+  const [forecastWeather, setForecastWeather] = useState([]);
 
-  const getWeather = () => {
+  const getCurrentWeather = () => {
       navigator.geolocation.getCurrentPosition(function (position) {
         fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=23e2d9191ae9c18f66d5ef2ebd1225f7`
+          `${WEATHER_API_URL}/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${WEATHER_API_KEY}&units=metric`
         )
           .then((res) => res.json())
-          .then((data) => {
-            setWeather(data);
+         .then((data) => {
+            setCurrentWeather(data);
           });
       });
     
   };
 
-  console.log(weather);
+  console.log(currentWeather);
 
   useEffect(() => {
-    getWeather();
+    getCurrentWeather();
   }, []);
+
+  const getForecastWeather = () => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      fetch(
+        `${WEATHER_API_URL}/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${WEATHER_API_KEY}&units=metric`
+      )
+        .then((res) => res.json())
+       .then((data) => {
+          setForecastWeather(data);
+        });
+    });
+  
+};
+
+console.log(forecastWeather);
+
+useEffect(() => {
+  getForecastWeather();
+}, []);
 
   return (
     <div className="app">
       <h1>Using geolocation JavaScript API in React</h1>
-      <Button getWeather={getWeather} />
+      <Button getCurrentWeather={getCurrentWeather} />
     </div>
   );
 };
---
+++ src/api.js
@@ -0,0 +1,2 @@
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "23e2d9191ae9c18f66d5ef2ebd1225f7";
