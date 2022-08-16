e4fa6b1287fb7ffb1195d1322a93373706565d41
awo-ali <awoali@hotmail.co.uk>
2022-08-16 10:05:53 +0100

    fetching data, styling app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from "react";
 import "./App.scss";
-import Button from "./component/Button/Button";
-import { WEATHER_API_URL, WEATHER_API_KEY} from './api';
-
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import CurrentWeather from "./component/CurrentWeather/CurrentWeather";
+import ThreeHourForecast from "./component/ThreeHourForecast/ThreeHourForecast";
+import ForecastList from "./component/ForecastList/ForecastList";
+// import ForecastCard from "./component/ForecastCard/ForecastCard";
 
 const App = () => {
-
   const [currentWeather, setCurrentWeather] = useState([]);
+  const [currentCity, setCurrentCity] = useState([]);
+  const [currentWeatherMain, setCurrentWeatherMain] = useState([]);
   const [forecastWeather, setForecastWeather] = useState([]);
 
-  const getCurrentWeather = () => {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        fetch(
-          `${WEATHER_API_URL}/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${WEATHER_API_KEY}&units=metric`
-        )
-          .then((res) => res.json())
-         .then((data) => {
-            setCurrentWeather(data);
-          });
-      });
-    
+
+  const getCurrentWeather = async () => {
+    navigator.geolocation.getCurrentPosition(async function (position) {
+      const response = await fetch(
+        `${WEATHER_API_URL}/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${WEATHER_API_KEY}&units=metric`
+      );
+      const data = await response.json();
+      setCurrentWeather(data.weather[0]);
+      setCurrentWeatherMain(data.main);
+      setCurrentCity(data.name);
+    });
   };
 
   console.log(currentWeather);
@@ -28,29 +31,39 @@ const App = () => {
     getCurrentWeather();
   }, []);
 
+  console.log(currentWeatherMain);
+  console.log(currentCity);
+
   const getForecastWeather = () => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      fetch(
+    navigator.geolocation.getCurrentPosition(async function (position) {
+      const response = await fetch(
         `${WEATHER_API_URL}/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${WEATHER_API_KEY}&units=metric`
-      )
-        .then((res) => res.json())
-       .then((data) => {
-          setForecastWeather(data);
-        });
+      );
+      const data = await response.json();
+      setForecastWeather(data.list);
     });
-  
-};
+  };
 
-console.log(forecastWeather);
+  console.log(forecastWeather);
 
-useEffect(() => {
-  getForecastWeather();
-}, []);
+
+  useEffect(() => {
+    getForecastWeather();
+  }, []);
 
   return (
     <div className="app">
-      <h1>Using geolocation JavaScript API in React</h1>
-      <Button getCurrentWeather={getCurrentWeather} />
+      <h1>Welcome to weatherly !!!</h1>
+      {currentWeather && (
+        <CurrentWeather
+          weather={currentWeather}
+          temp={currentWeatherMain}
+          city={currentCity}
+        />
+      )}
+      <ThreeHourForecast />
+      {/* <ForecastCard forecast={forecastWeather}/> */}
+      <ForecastList forecast={forecastWeather} />
     </div>
   );
 };
